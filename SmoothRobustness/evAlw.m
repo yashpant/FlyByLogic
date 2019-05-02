76cd06e9fbcec2c552012c9884779e21e1cc09ee
nellro <alena.rodionova@tuwien.ac.at>
2019-05-01 20:44:48 -0400

    tests ov evAlw and alwEv

--- SmoothRobustness/evAlw.m
+++ SmoothRobustness/evAlw.m
@@ -1,6 +1,6 @@
 function [robustness, robustness_smooth] = evAlw(states, P, I, J, C)
 for i = 1:length(I)
-    % time point in which we check 'always'
+    % i: time point in which we check 'always'
     % I may start with 0, since it is time increments, steps in the future
     % Since MATLAB indices start with 1, we do 1+I(i)
     t = 1+I(i);
